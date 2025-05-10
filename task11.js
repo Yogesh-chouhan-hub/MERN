0277dc9ce045a3e21faf8c5669937c50d900c4a5
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2025-05-10 23:25:15 +0530

    Add files via upload

---
+++ task11.js
@@ -0,0 +1,31 @@
+  let arr=[ ];
+
+      function addTask(){
+         let val = document.getElementById('inp');
+         let date = document.getElementById('dt');
+         let store = val.value;
+         let data = date.value;
+         if(store !== ""){
+            arr.push({task: store,date: data});
+            val.value = "";
+            date.value ="";
+            renderTask();
+         }
+      }
+
+      function renderTask(){
+         let html="";
+         for(let i=0;i<arr.length;i++){
+            html+=`<p id='ptag'><span>${arr[i].task}</span><span>${arr[i].date}</span>
+                   <span><button id='but1' onclick='deleteTask(${i})'>Delete</button></span>
+                   </p>`;
+         }
+
+         document.getElementById('container').innerHTML=html;
+      }
+
+      function deleteTask(index){
+         arr.splice(index,1);
+         renderTask();
+      }
+  
